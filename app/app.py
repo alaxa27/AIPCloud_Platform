e8d5f28263727085c195e4a5f57bc073a7d310d4
SAKMECHE Taqiy <alaxa2@gmail.com>
2017-08-01 18:30:15 +0200

    Added cross domain support for all routes

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ from src.authentication import Authentication
 from src.initialize_db import InitializeDB
 from src.snippets.snippets import dialA, dialB
 from flask import abort, request, jsonify, g, url_for
+from flask_cors import CORS, cross_origin
 from src.aipcloud.text import sentiment
 from src.aipcloud.image import classifier, utils
 import cv2
@@ -15,6 +16,8 @@ from time import time
 import subprocess
 from urllib import (request as rqst, error)
 
+app = CORS(app)
+
 
 @auth.verify_password
 def verify_password(email_or_token, password):
@@ -22,17 +25,17 @@ def verify_password(email_or_token, password):
     return authenticate.verify_password(email_or_token, password)
 
 
-# @app.route('/init')
-# def initialization():
-#     try:
-#         print("-------------------------->>No database.")
-#         InitializeDB(db)
-#         import nltk
-#         nltk.download("punkt")
-#         return "200"
-#     except:
-#         if not os.path.exists('database/db.sqlite'):
-#             return "400"
+@app.route('/init')
+def initialization():
+    try:
+        print("-------------------------->>No database.")
+        InitializeDB(db)
+        import nltk
+        nltk.download("punkt")
+        return "200"
+    except:
+        if not os.path.exists('database/db.sqlite'):
+            return "400"
 
 
 @app.route('/token')
