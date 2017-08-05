a861e72e690dab34c6417e2e4e955a1dec3f8008
SAKMECHE Taqiy <alaxa2@gmail.com>
2017-08-05 13:55:28 +0200

    Standardized /analyze/extraction JSON response v2

--- app/app.py
+++ app/app.py
@@ -149,7 +149,7 @@ def keywords_extraction():
     keywords = keywords.extract(text, keywordCount=8, verbose=True)
     data = []
     for key in keywords:
-        data.append({"keyword": key[0], "score": round(key[1])})
+        data.append({"keyword": key[0], "score": round(key[1], 4)})
     return jsonify({"keywords": data})
 
 
