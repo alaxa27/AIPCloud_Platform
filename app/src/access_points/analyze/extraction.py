2e0b60793d580b57104a5449d78c970d6b046669
OsamaMM <extra.time.om@gmail.com>
2017-08-08 18:42:29 +0100

    set extraction access point

---
+++ app/src/access_points/analyze/extraction.py
@@ -0,0 +1,39 @@
+from flask import abort, jsonify
+from ...instances_aipcloud import load_keywords_instance
+
+
+
+def extract(text, sentimentBool, volume):
+    try:
+        if text is None:
+            abort(400)
+        if volume is None:
+            volume = 8
+        else:
+            volume = float(volume)
+        if sentimentBool:
+            sentimentBool = int(sentimentBool)
+
+        keywords = load_keywords_instance()
+        keywords = keywords.extract(text, keywordCount=volume, verbose=True)
+        data = []
+        for key in keywords:
+            if  sentimentBool:
+                if int(sentimentBool):
+                    #callsentiment
+
+                    data.append({"keyword": key[0], "score": round(key[1], 4), "sentiment": {
+                        "positivity": 45.7,
+                        "neutrality": 39,
+                        "negativity": 15.3,
+                        "relevance": 62.6
+
+                    }})
+                    #Get sentiment from eachword
+                    pass
+            else:
+                data.append({"keyword": key[0], "score": round(key[1], 4)})
+
+        return jsonify(data)
+    except Exception as e:
+        abort(500, e)
