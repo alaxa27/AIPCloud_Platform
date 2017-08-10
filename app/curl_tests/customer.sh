8341710f9cadb6589f3c5952f3110dddb1460955
OsamaMM <extra.time.om@gmail.com>
2017-08-10 21:42:35 +0100

    adapt curl queries to change: sentence to text

--- app/curl_tests/customer.sh
+++ app/curl_tests/customer.sh
@@ -2,40 +2,40 @@
 
 echo '================= 1 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 2 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 3 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 4 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 5 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 6 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 7 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 8 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 9 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
 
 echo '================= 10 ==================='
 
-curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"sentence":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
+curl -u text@jdc.fr:jdc -i -X POST -H "Content-Type: application/json" -d '{"text":"Votre service est très mauvais, aucune écoute, aucune considération. Bref, encore un SAV bien inutile."}' http://0.0.0.0:80/analyze/customer
