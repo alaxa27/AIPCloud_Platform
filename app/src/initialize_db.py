68a0fc24832344a086095f1f4821bf6e00a8a8df
OsamaMM <extra.time.om@gmail.com>
2017-08-12 00:07:21 +0100

    update db initialization

--- app/src/initialize_db.py
+++ app/src/initialize_db.py
@@ -7,7 +7,7 @@ class InitializeDB(object):
         # Create the database
         db.create_all()
         # Add 2 users
-        user1 = User(email='text@jdc.fr', admin=True)
+        user1 = User(email='text@jdc.fr', admin=False)
         user1.hash_password('jdc')
         user2 = User(email='image@jdc.fr')
         user2.hash_password('junior')
@@ -18,33 +18,26 @@ class InitializeDB(object):
         db.session.add(user3)
         db.session.commit()
         # Add acces points to the data base
-        paths = ['/analyze/sentence',
+        paths = ['/analyze/word',
+                 '/analyze/sentence',
                  '/analyze/text',
                  '/analyze/customer',
                  '/analyze/dialogue',
                  '/analyze/extraction',
                  '/analyze/image']
         for path in paths:
-            db.session.add(AccessPoint(path=path))
+            if path == '/analyze/dialogue':
+                db.session.add(AccessPoint(path=path, method='GET'))
+            else:
+                db.session.add(AccessPoint(path=path))
             db.session.commit()
-        # Grant access to text methods for the first user
+        # Grant access to text methods for the first user and for the admin
         l = paths[:-1]
         for path in l:
-            auth = Authorization(timeref = int(time()) + 86400)
             ap = AccessPoint.query.filter_by(path=path).first()
-            auth.point = ap
-            user1.grant_access_to(auth)
-            db.session.commit()
-        # Grant access to image method for the second user
-        auth = Authorization(timeref = int(time()) + 86400)
+            user1.grant_access_to(ap)
+            user3.grant_access_to(ap)
+        # Grant access to image method for the second user and for the admin
         ap = AccessPoint.query.filter_by(path='/analyze/image').first()
-        auth.point = ap
-        user2.grant_access_to(auth)
-        db.session.commit()
-        # Grant access to all methods for the admin
-        for path in paths:
-            auth = Authorization(timeref = int(time()) + 86400)
-            ap = AccessPoint.query.filter_by(path=path).first()
-            auth.point = ap
-            user3.grant_access_to(auth)
-            db.session.commit()
+        user2.grant_access_to(ap)
+        user3.grant_access_to(ap)
