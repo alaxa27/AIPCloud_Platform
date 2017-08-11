c694a809a82facb53bb8f0df879ad8565499b257
OsamaMM <extra.time.om@gmail.com>
2017-08-11 16:28:59 +0100

    catch eventual exceptions

--- app/src/access_points/grantaccess.py
+++ app/src/access_points/grantaccess.py
@@ -1,17 +1,15 @@
-from ..models import User, AccessPoint, Authorization
-from .. import db
-from flask import abort, jsonify
-from time import time
+from ..models import User, AccessPoint
+from flask import abort
 
 
 def grant(email, route):
     if not email or not route:
         abort(400, 'You have to precise the user\'s email as well as the route of the access point.')
-    # try:
-    user = User.query.filter_by(email=email).first()
-    ap = AccessPoint.query.filter_by(path=route).first()
-    if user.grant_access_to(ap):
-        return "The access to '{}' for '{}' is successfully updated.".format(route, email), 200
-    return "The access to '{}' is successfully granted for '{}'.".format(route, email), 201
-    # except Exception as e:
-    #     abort(500, e)
+    try:
+        user = User.query.filter_by(email=email).first()
+        ap = AccessPoint.query.filter_by(path=route).first()
+        if user.grant_access_to(ap):
+            return "The access to '{}' for '{}' is successfully updated.".format(route, email), 200
+        return "The access to '{}' is successfully granted for '{}'.".format(route, email), 201
+    except Exception as e:
+        abort(500, e)
