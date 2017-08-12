28b9e4b5f165a4839ed619c7594fb7c9f6cba2c8
OsamaMM <extra.time.om@gmail.com>
2017-08-12 21:57:43 +0100

    update db initialization

--- app/src/initialize_db.py
+++ app/src/initialize_db.py
@@ -7,17 +7,20 @@ class InitializeDB(object):
         # Create the database
         db.create_all()
         # Add 2 users
-        user1 = User(email='text@jdc.fr', admin=False)
+        user1 = User(email='text@jdc.fr', admin=True)
         user1.hash_password('jdc')
         user2 = User(email='image@jdc.fr')
-        user2.hash_password('junior')
+        user2.hash_password('jdc')
         user3 = User(email='admin@jdc.fr', admin=True)
         user3.hash_password('jdc')
+        user4 = User(email='client@jdc.fr')
+        user4.hash_password('jdc')
         db.session.add(user1)
         db.session.add(user2)
         db.session.add(user3)
+        db.session.add(user4)
         db.session.commit()
-        # Add acces points to the data base
+        # Add access points to the data base
         paths = ['/analyze/word',
                  '/analyze/sentence',
                  '/analyze/text',
@@ -31,12 +34,13 @@ class InitializeDB(object):
             else:
                 db.session.add(AccessPoint(path=path))
             db.session.commit()
-        # Grant access to text methods for the first user and for the admin
+        # Grant access to text methods for the first and the fourth users and for the admin
         l = paths[:-1]
         for path in l:
             ap = AccessPoint.query.filter_by(path=path).first()
             user1.grant_access_to(ap)
             user3.grant_access_to(ap)
+            user4.grant_access_to(ap)
         # Grant access to image method for the second user and for the admin
         ap = AccessPoint.query.filter_by(path='/analyze/image').first()
         user2.grant_access_to(ap)
