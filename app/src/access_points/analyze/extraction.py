646cbe490ec003239e698f6723fadc9c22b21f21
SAKMECHE Taqiy <alaxa2@gmail.com>
2017-08-13 18:39:59 +0200

    Unknown error inextraction. Corrected with str()

--- app/src/access_points/analyze/extraction.py
+++ app/src/access_points/analyze/extraction.py
@@ -24,11 +24,11 @@ def extract(text, sentimentBool, volume, keywords, sentenceAnalyzer):
 
                     data.append({
                     "keyword": key[0],
-                    "score": round(key[1], 4),
+                    "score": str(round(key[1], 4)),
                     "sentiment": {
-                        'positivity': round(results[2], 4),
-                        'neutrality': round(results[1], 4),
-                        'negativity': round(results[0], 4)
+                        'positivity': str(round(results[2], 4)),
+                        'neutrality': str(round(results[1], 4)),
+                        'negativity': str(round(results[0], 4))
                     }
                     })
                     # Get sentiment from eachword
