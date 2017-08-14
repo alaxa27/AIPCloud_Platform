e9eca250a8b0b1294fcb5dd28c4317d7df75656c
SAKMECHE Taqiy <alaxa2@gmail.com>
2017-08-14 02:40:20 +0200

    Stupid error: aggressivity vs agressivity

--- app/src/access_points/analyze/customer.py
+++ app/src/access_points/analyze/customer.py
@@ -7,7 +7,7 @@ def analyzer(text, textCS):
         #analysis = textCS.analyze(text)
         #results = analysis['res']
         data = {'sentiment': 0.3423,
-                'aggressivity': 0.2167,
+                'agressivity': 0.2167,
                 'refund': 0.4521}
         g.user.save_query('/analyze/customer', "none", 1)
         return jsonify(data)
