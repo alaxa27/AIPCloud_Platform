7f8556552508446a8f5b3ca4fa13e4084d239d8e
SAKMECHE Taqiy <alaxa2@gmail.com>
2017-09-11 21:43:36 +0200

    Created an install script and changed the api domain

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+apt-get -y update && apt-get -y upgrade
+apt-get -y install vim docker.io
+
+curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+chmod +x /usr/local/bin/docker-compose
+
+git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
+cd /opt/letsencrypt
+./letsencrypt-auto certonly --standalone -d api.aipcloud.io
+
+cd
+cd AIPCloud_Platform
+docker-compose build
