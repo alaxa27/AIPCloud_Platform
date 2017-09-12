86665fd42639608c07a5fd9b310948ea994dc028
SAKMECHE Taqiy <alaxa2@gmail.com>
2017-09-12 19:21:25 +0200

    Timeref is not mandatory

--- app/src/access_points/user/add.py
+++ app/src/access_points/user/add.py
@@ -11,13 +11,16 @@ def add(email, password, adminBool, access_points):
             user = User(email=email, admin=True)
         else:
             user = User(email=email, admin=False)
-            
+
         user.hash_password(password)
         user.save_user()
         if type(access_points) is list:
             for ap in access_points:
                 path = ap['path']
-                timeref = ap['timeref']
+                try:
+                    timeref = ap['timeref']
+                except:
+                    timeref = None
                 user.grant_access_to(path, timeref)
         return "User with email '{}' is successfully created.".format(email), 201
     except Exception as e:
