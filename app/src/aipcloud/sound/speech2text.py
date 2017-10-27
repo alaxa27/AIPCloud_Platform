02a1698f5ea45b2793d84886171f537a5c5c7f5a
SAKMECHE Taqiy <alaxa2@gmail.com>
2017-10-27 23:06:50 +0200

    We had so many problems. The problem came from google-cloud python's doc

--- app/src/aipcloud/sound/speech2text.py
+++ app/src/aipcloud/sound/speech2text.py
@@ -11,9 +11,10 @@ from google.cloud import storage
 class Speech2Text():
     def __init__(self):
         self.storageClient = storage.Client()
+        self.bucket_name = 'aipcloud-bucket'
         # Get bucket:
         try:
-            self.bucket = self.storageClient.get_bucket('aipcloud-bucket')
+            self.bucket = self.storageClient.get_bucket(self.bucket_name)
         except google.cloud.exceptions.NotFound:
             raise Exception('Sorry, that bucket does not exist!')
         # Instantiates a client
@@ -27,28 +28,37 @@ class Speech2Text():
         timeS = time.time()
 
         # UPLOAD THE FILE TO GCS
-        blob = self.bucket.blob(str(uuid4()))
+        fileName = str(uuid4()) + '.raw'
+        blob = self.bucket.blob(fileName)
         blob.upload_from_filename(filePath)
         # Make sure the file has a unique name
         # Then feed the analyzer with the URI provided by GCS
-        uri = blob.self_link
-        audio = types.RecognitionAudio(uri='gs://' + uri)
+        uri = 'gs://' + self.bucket_name + '/' + fileName#blob.self_link
 
-        # Detects speech in the audio file
-        response = self.client.long_running_recognize(self.config, audio)
-        #Delete file from GCS
-        blob.delete()
+        audio = types.RecognitionAudio(uri=uri)
 
-        if len(response.results) == 0:
-            raise Exception('The server did not send any recognition results.')
-        alternatives = response.results[0].alternatives
+        try:
+            # Detects speech in the audio file
+            operation = self.client.long_running_recognize(config=self.config, audio=audio)
 
-        results = []
+            retry_count = 50
+            while retry_count > 0 and not operation.done():
+                retry_count -= 1
+                time.sleep(1)
 
-        for res in response.results:
-            results.append({
-                'transcript': res.alternatives[0].transcript,
-                'confidence': str(res.alternatives[0].confidence)
-            })
+            operationResults = operation.result().results
+            if len(operationResults) == 0:
+                raise Exception('The server did not send any recognition results.')
 
-        return {'res': results, 'exec_time': time.time() - timeS}
+            results = []
+            for res in operationResults:
+                results.append({
+                    'transcript': res.alternatives[0].transcript,
+                    'confidence': str(res.alternatives[0].confidence)
+                })
+
+            blob.delete()
+            return {'res': results, 'exec_time': time.time() - timeS}
+        except:
+            #Delete file from GCS
+            blob.delete()
